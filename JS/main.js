2924e4404d3f90578b3f479132ceb9453b6ddd65
unknown <mahmouddoma.com>
2022-07-14 07:31:56 +0200

    sec commit

--- JS/main.js
+++ JS/main.js
@@ -8,25 +8,25 @@ let shopItemsData = [
 		name: "Casual Shirt",
 		price: 50,
 		desc: "Lorem ipsum dolor sit amet consectetur adipisicing.",
-		img: "img/ OIP (1).jfif",
+		img: "img/img 1.jfif",
  	}, {
 		id: "dasd",
 		name: "Casual",
 		price: 60,
 		desc: "Lorem ipsum dolor sit amet consectetur adipisicing.",
-		img: "img/OIP (2).jfif",
+		img: "img/img .jfif",
 	}, {
 		id: "asfas",
 		name: "Shirt",
 		price: 30,
 		desc: "Lorem ipsum dolor sit amet consectetur adipisicing.",
-		img: "img/OIP .jfif",
+		img: "img/img 3.jfif",
 	}, {
 		id: "sadas",
 		name: "Classic",
 		price: 1500,
 		desc: "Lorem ipsum dolor sit amet consectetur adipisicing.",
-		img: "img/ R (1).jfif",
+		img: "img/img 4.jfif",
 	}
 ]
 
